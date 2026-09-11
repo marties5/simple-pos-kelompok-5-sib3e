36cf50ad0a3001528182fe3ec26a47619016a125
taniaeka <taniaaaep@gmail.com>
2026-09-11 13:21:07 +0700

    Tambah badge stok menipis

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -22,8 +22,19 @@
             @foreach ($products as $product) 
                 <div class="border rounded-md p-3 cursor-pointer" 
                     @click="addToCart({{ $product->id }}, '{{ $product->name }}', {{ $product->price }})"> 
+                    
                     <p class="font-medium">{{ $product->name }}</p> 
-                    <p class="text-sm text-slate-500">Rp {{ number_format($product->price) }}</p> 
+                    
+                    <p class="text-sm text-slate-500">
+                        Rp {{ number_format($product->price) }}
+                    </p>
+
+                    @if ($product->stock < 10)
+                        <span class="inline-block text-xs px-2 py-1 mt-1 rounded bg-amber-100 text-amber-700">
+                            Stok Menipis
+                        </span>
+                    @endif
+
                 </div> 
             @endforeach 
         </div> 
@@ -32,6 +43,7 @@
             <template x-for="item in cart" :key="item.id"> 
                 <div class="flex items-center gap-3 mb-2">
                     <p x-text="item.name + ' - Rp ' + item.price"></p>
+                    
                     <button 
                         type="button"
                         @click="removeFromCart(item.id)"
